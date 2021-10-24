6e5015afa587e78d080042681718e1f379c545ae
Emmanuel289 <emmirald@gmail.com>
2021-10-23 20:04:25 -0600

    Added an h1 element to index.html

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  React.createElement(
+    "h1",
+    {
+      style: { color: "blue", fontFamily: "Sans-Serif" },
+    },
+    "This is an h1 element"
+  ), // first arg in render() is the element that we want to create
+  document.getElementById("root") // second arg in render() is where we want to place the created element in the index.html file
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
