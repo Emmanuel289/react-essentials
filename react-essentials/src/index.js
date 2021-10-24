41f89836776e903ef91484d464e3b517db4fe1b5
Emmanuel289 <emmirald@gmail.com>
2021-10-23 20:29:59 -0600

    Refactored file to create an unordered list using JSX syntax

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// First arg in render() is the element that we want to create and the Second arg in render() is where we want to place the created element in the index.html file
+
 ReactDOM.render(
-  React.createElement(
-    "h1",
-    {
-      style: { color: "blue", fontFamily: "Sans-Serif" },
-    },
-    "This is an h1 element"
-  ), // first arg in render() is the element that we want to create
-  document.getElementById("root") // second arg in render() is where we want to place the created element in the index.html file
+  <div>
+    <p className="main">
+      Are you about the four-day work week? You only work on the following days:
+    </p>
+    <ul className="days">
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+      <li>Thursday</li>
+    </ul>
+    <p>And your weekend begins on Friday! Yay!</p>
+  </div>,
+  document.getElementById("root")
 );
