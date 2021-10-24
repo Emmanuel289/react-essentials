d821cc7cbea113698fe501b5d68177a143691932
Emmanuel289 <emmirald@gmail.com>
2021-10-23 21:53:35 -0600

    Removed redundant code

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
 // First arg in render() is the element that we want to create and the Second arg in render() is where we want to place the created element in the index.html file
 
-ReactDOM.render(
-  <div>
-    <p className="main">
-      Are you about the four-day work week? You only work on the following days:
-    </p>
-    <ul className="days">
-      <li>Monday</li>
-      <li>Tuesday</li>
-      <li>Wednesday</li>
-      <li>Thursday</li>
-    </ul>
-    <p>And your weekend begins on Friday! Yay!</p>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App></App>, document.getElementById("root"));
