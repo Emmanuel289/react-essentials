4422112e5f6619442816a32fe178fc44655cc6a2
Emmanuel289 <emmirald@gmail.com>
2021-10-24 17:56:13 -0600

    Added an authorization property for conditionally rendering components

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -5,4 +5,4 @@ import App from "./App";
 
 // First arg in render() is the element that we want to create and the Second arg in render() is where we want to place the created element in the index.html file
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App authorized={true} />, document.getElementById("root"));
