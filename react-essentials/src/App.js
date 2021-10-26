ed30e98a7e86addbcc4e95affeaf6d73703b3a34
Emmanuel289 <emmirald@gmail.com>
2021-10-26 17:15:16 -0600

    Display data about Garage

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Car from "./car.jpg";
+
+const url = `https://api.github.com/users/`;
 
 function App({ login }) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
+    fetch(url + `${login}`)
       .then((response) => response.json())
       .then(setData);
   }, []);
 
   if (data) {
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div className="App">
+        <h1>Welcome to {data.name}'s Garage</h1>
+        <p>We are located at 251 Scenic Way NW {data.location}</p>
+        <img alt="black porsche car on the highway" src={Car} height="200px" />
+      </div>
+    );
   }
-  return <div>No User Available</div>;
+  return <div>No User Data Available</div>;
 }
-
 export default App;
