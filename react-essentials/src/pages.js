016a5aadd0406a90fd73db179e14d4eab77f2feb
Emmanuel289 <emmirald@gmail.com>
2021-10-26 21:56:52 -0600

    Components for rendering pages

---
+++ react-essentials/src/pages.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+/**Pages.js contains components for different pages of the application */
+
+function Home() {
+  return (
+    <div>
+      <h1>[Company Website]</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>[About]</h1>
+    </div>
+  );
+}
+
+function Events() {
+  return (
+    <div>
+      <h1>[Events]</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>[Contact]</h1>
+    </div>
+  );
+}
+
+export { Home, About, Events, Contact };
